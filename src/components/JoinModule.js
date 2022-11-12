081b59650604d2136808600d5a78d7b190d71244
Alex Sánchez C <asmalexander@hotmail.com>
2022-11-11 23:29:51 -0600

    Modulo de unirse a partidas
    Se añadio el modulo para unirse a partidas

---
+++ src/components/JoinModule.js
@@ -0,0 +1,8 @@
+export default function JoinModule(props) {
+    return (
+        <div className="joinModule">
+            <div className="titulo2"></div>
+            <div className="squareSelection"></div>
+        </div>
+    )
+}
